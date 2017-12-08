1f8a1741d5eae7b2ea765f70ef752014751b7c7b
Xiao Lin <xilin95@Xiaos-MacBook-Pro.local>
2017-12-08 13:04:11 -0800

    added thirdMax.java

---
+++ thirdMax.java
@@ -0,0 +1,50 @@
+/*
+Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+Input: [3, 2, 1]
+
+Output: 1
+
+Explanation: The third maximum is 1.
+Example 2:
+Input: [1, 2]
+
+Output: 2
+
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+Input: [2, 2, 3, 1]
+
+Output: 1
+
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+*/
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        Integer max = null;
+        Integer secondMax = null;
+        Integer thirdMax = null;
+
+        for (Integer n: nums) {
+            if (n.equals(max) || n.equals(secondMax) || n.equals(thirdMax)) {
+                continue;
+            }
+
+            if (max == null || n > max) {
+                thirdMax = secondMax;
+                secondMax = max;
+                max = n;
+            } else if (secondMax == null || n > secondMax) {
+                thirdMax = secondMax;
+                secondMax = n;
+            } else if (thirdMax == null || n > thirdMax) {
+                thirdMax = n;
+            }
+        }
+
+        return thirdMax == null? max : thirdMax;
+    }
+}
